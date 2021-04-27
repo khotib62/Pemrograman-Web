0ab9502dfdd79f5c90c8ad4a3fea66e47573dc28
Zidane071 <79822989+Zidane071@users.noreply.github.com>
2021-04-27 23:40:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css2?family=Hind:wght@500&family=Poppins&family=Zen+Dots&display=swap" rel="stylesheet">        <!-- header -->
         <header id="home">
             <nav>
@@ -135,8 +136,44 @@
             <!-- endofcontact -->
 
             <!-- footer -->
-            <footer>
+            <footer class="site-footer">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <h6>About</h6>
+                        <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+                    </div>
 
+                    <div class="col-xs-6 col-md-3">
+                        <h6>Categories</h6>
+                        <ul class="footer-links">
+                        <li>Html</li>
+                        <li>Css</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        </ul>
+                    </div>
+
+                    <div class="col-xs-6 col-md-3">
+                        <h6>Contact Person</h6>
+                        <ul class="footer-links">
+                        <li><a href="https://wa.me/6285245973043">Khotibul Ummam</a></li>
+                        <li><a href="https://wa.me/6285156537607">Zidane Putra Sanjaya</a></li>
+                        <li><a href="https://wa.me/6281236211917">Fauzi Ramadhan</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                    <hr>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-sm-6 col-xs-12">
+                            <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
+                        <a href="https://github.com/khotib62/Pemrograman-Web.git">Apollo69 Project</a>.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </footer>
             <!-- endoffooter -->
 
